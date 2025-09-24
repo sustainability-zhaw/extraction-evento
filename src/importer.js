8a86759599e2b864e47326be469db7b8b9ee0623
Davide Stallone <stlo@zhaw.ch>
2025-09-24 20:28:52 +0200

    simple heuristics to build module abstract

--- src/importer.js
+++ src/importer.js
@@ -13,8 +13,12 @@ function createPrintURL(url) {
 async function getModuleURLs() {
   const config = getConfig();
   const eventoSearchURL = createPrintURL(config.eventoSearchUrl);
-  const content = await fetch(eventoSearchURL).then((resp) => resp.text());
+  const response = await fetch(eventoSearchURL);
 
+  if (response.status !== 200) 
+    throw `Responded with ${response.status}`;
+
+  const content = await response.text();
   const dom = new JSDOM(content);
   const elements = dom.window.document.querySelectorAll(
     "tr.result-row > td:nth-of-type(1) > span:nth-of-type(1) > a:nth-of-type(2)"
@@ -29,8 +33,81 @@ async function getModuleURLs() {
   return urls;
 }
 
+function parseModuleId(labels) {
+  const moduleId = labels.find(element => element.textContent === "Nr.")?.nextElementSibling?.textContent;
+  let departement;
+  let level;
+
+  if (moduleId) {
+    const values = moduleId.split(".");
+    if (values.length > 0) departement = values[0].toUpperCase();
+    if (values.length > 1) level = values[1];
+  }
+
+  return { moduleId, departement, level }
+}
+
+function parseVersion(dom, document) {
+  const xpathResult = document.evaluate(
+    "//i[contains(text(), 'Version: ')]",
+    document,
+    null,
+    dom.window.XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+    null
+  );
+
+  if (xpathResult.snapshotLength > 0) {
+    const element = xpathResult.snapshotItem(0);
+    const result = /^Version: (?<version>\d+\.\d+)/.exec(element.textContent);
+    if (result.groups?.version) {
+      return result.groups.version;
+    }
+  }
+}
+
+function parseAbstract(document) {
+  return Array.from(document.querySelectorAll("table tbody tr td:first-child")).reduce((acc, element) => {
+    if(element.textContent === "" || /beschrieb|inhalt/i.test(element.textContent)) {
+      acc += element.nextElementSibling?.textContent ?? "";
+    }
+    return acc;
+  }, "");
+}
+
 async function importModule(url) {
-  const content = await fetch(createPrintURL(url)).then((resp) => resp.text());
+  url = createPrintURL(url);
+  const response = await fetch(url);
+
+  if (response.status !== 200)
+      throw `Responded with ${response.status}`;
+
+  const content = await response.text();
+  const dom = new JSDOM(content);
+  const document = dom.window.document;
+
+  const labels = Array.from(document.querySelectorAll(".detail-label"));
+
+  const eventoId = url.searchParams.get("IDAnlass");
+  const { moduleId, departement, level } = parseModuleId(labels);
+  const title = labels.find(element => element.textContent === "Bezeichnung")?.nextElementSibling?.textContent;
+  const organizer = labels.find(element => element.textContent === "Veranstalter")?.nextElementSibling?.textContent;
+  const credits = labels.find(element => element.textContent === "Credits")?.nextElementSibling?.textContent;
+  const version = parseVersion(dom, document);
+  const abstract = parseAbstract(document);
+
+  // TODO: upsert infoObject
+
+  console.log({
+    eventoId,
+    moduleId,
+    departement,
+    level,
+    title,
+    organizer,
+    credits,
+    version,
+    abstract
+  })
 }
 
 export async function run() {
