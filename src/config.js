bff607c717a87b9314b0b073f54a13f6c5a0b7af
Davide Stallone <stlo@zhaw.ch>
2025-09-24 20:29:08 +0200

    add mq config

--- src/config.js
+++ src/config.js
@@ -5,8 +5,13 @@ let config =  {
   logLevel: 4,
   dbHost: "localhost:8080",
   eventoSearchUrl: "http://localhost:3000",//"https://eventoweb.zhaw.ch/Evt_Pages/SuchResultat.aspx?node=c594e3e5-cd9a-4204-9a61-de1e43ccb7b0&Tabkey=WebTab_ModuleSuchenZHAW",
-  batchSize: 25,
-  importInterval: 86400 // 24h
+  batchSize: 5,
+  importInterval: 86400, // 24h
+  mqHost: "mq",
+  mqUser: "extraction-evento",
+  mqPass: "guest",
+  mqExchange: "zhaw-km",
+  mqHeartbeat: 6000,
 };
 
 const configPaths = ["/etc/app/config.json", "/etc/app/secrets.json"];
